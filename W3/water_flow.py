e74862f067c651042d9b70ca6295dadb8e6045c8
Agustin <82238717+AgU131@users.noreply.github.com>
2025-02-08 18:43:35 -0300

    Add files via upload

---
+++ W3/water_flow.py
@@ -0,0 +1,19 @@
+"""
+I should get this
+> python test_water_flow.py
+======================== test session starts ==========================
+platform win32 -- Python 3.11.1, pytest-7.2.1, pluggy-1.0.0 --
+rootdir: C:\Users\cse111\week03
+collected 3 items
+test_water_flow.py::test_water_column_height PASSED              [ 33%]
+test_water_flow.py::test_pressure_gain_from_water_height PASSED  [ 66%]
+test_water_flow.py::test_pressure_loss_from_pipe PASSED          [100%]
+========================= 3 passed in 0.07s ===========================
+"""
+import math as m
+
+def pressure_loss_from_fittings(fluid_velocity, quantity_fittings):
+    P = (-0.04*/)
+−0.04 ρ v2 n
+2000 
+
